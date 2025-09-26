21e2e0bbb7d6f3eabfa4eeb5e244f7a6e0b00545
Manu <109155188+Manolilloo@users.noreply.github.com>
2025-09-26 09:32:41 +0200

    hey

---
+++ Relación 1.2/src/BucleDoWhile.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BucleDoWhile {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe una contraseña.");
+
+        String
+    }
+}
--- Relación 1.2/src/Ej4.java
+++ Relación 1.2/src/Ej4.java
@@ -4,3 +4,9 @@ public class Ej4 {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un número positivo");
+
+        int num = sc.nextInt();
+
+        while (num <= 0) {
+
+        }
