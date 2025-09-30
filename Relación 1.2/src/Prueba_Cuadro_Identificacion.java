9c1abf9b237da268af4d8c6827b12a3b533ac0be
Manu <109155188+Manolilloo@users.noreply.github.com>
2025-09-30 10:24:43 +0200

    hey

---
+++ Relación 1.2/src/Prueba_Cuadro_Identificacion.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Prueba_Cuadro_Identificacion {
+    static void main(String[] args) {
+        String nombre;
+        int edad;
+
+        nombre = JOptionPane.showInputDialog(null, "Introduce tu nombre: ");
+        edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce tu edad: "));
+        JOptionPane.showMessageDialog(null, "Nombre: " + nombre + "\n" + "Edad: " + edad);
+    }
+}
