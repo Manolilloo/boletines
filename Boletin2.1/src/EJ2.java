2b69ca12d305bf5c2f053b3605b70b93a7611dff
Manu <109155188+Manolilloo@users.noreply.github.com>
2025-10-01 10:10:20 +0200

    Cambios n.boletin

---
+++ Boletin2.1/src/EJ2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EJ2 {
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int base, exponente;
+
+        System.out.println("Dame la base: ");
+        base = sc.nextInt();
+        System.out.println("Dame el exponente: ");
+        exponente = sc.nextInt();
+
+
+        }
+
+
+
+
+        System.out.println("El resultado de la potencia es: ");
+
+    }
+}
