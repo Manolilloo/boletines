6ae111e565ea816f98eb9d4b5ca1e036ae12ba81
Manu <109155188+Manolilloo@users.noreply.github.com>
2025-10-08 10:21:36 +0200

    a

---
+++ Boletin 2.2 Extras/src/EJ3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EJ3 {
+    static void main(String[] args) {
+        final int M=1000;
+        final int D=500;
+        final int C=100;
+        final int L=50;
+        final int X=10;
+        final int V=5;
+        final int I=1;
+
+        Scanner scanner = new Scanner(System.in);
+        String numRomano;
+    }
+}
